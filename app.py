f2eb9f19d5c00fce173830f9bccbec50e7fdd08a
ansu7514 <ansu75140301@gmail.com>
2020-12-02 19:46:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,43 +17,12 @@ def lotto():
     return jsonify(lotto)
 
 def select_numbers(select_amount:int):
-    lotto = [1,2,3,4,5,6]
-
     ### 이곳을 같이 채워봐요
 
+    lotto = [1,2,3,4,5,6]
 
-    ## example 1
-    lotto = []
-    for i in range(select_amount):
 
-        while True:
-            select_num = random.randint(1, 45)
-
-            is_duplicated = False
-            for num in lotto:
-                if select_num == num:
-                    is_duplicated = True
-                    break
-
-            if is_duplicated:
-                continue
-            lotto.append(select_num)
-            break
-
-    ## example 2
-    '''
-    candidate_numbers = lotto_numbers[:]
-    lotto = []
-    for i in range(select_amount):
-        select_num = candidate_numbers.pop(random.randrange(len(candidate_numbers)))
-        lotto.append(select_num)
-    '''
-
-    ## example 3
-    '''
-    lotto = random.sample(lotto_numbers, select_amount)
-    '''
     return lotto
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080) 
+    app.run(host='0.0.0.0', port=8080)
